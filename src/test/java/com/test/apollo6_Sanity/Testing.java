1778536234e4518d454c6f642c6ff021a47a2b64
gayathrikk <gayathri@htic.iitm.ac.in>
2024-02-21 13:20:48 +0530

    update commit

--- src/test/java/com/test/apollo6_Sanity/Testing.java
+++ src/test/java/com/test/apollo6_Sanity/Testing.java
@@ -156,6 +156,26 @@ public class Testing {
 	 		    System.out.println("Human-44 not clicked: " + e.getMessage());
 	 		}
 	    	 checkConsoleLog();
+	    	 
+	    	 
+	    	 String parentWindow = driver.getWindowHandle();
+	    	 try {
+		 		    WebElement viewericon = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//nb-icon[@nbtooltip='Viewer']")));
+		 		   viewericon.click();
+		 		    System.out.println("viewer icon is clicked successfully.");
+		 		    Thread.sleep(2000);
+		 		} catch (Exception e) {
+		 		    System.out.println(" viewer icon is not clicked: " + e.getMessage());
+		 		}
+	    	 
+	    	 wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+			  Set<String> allWindows = driver.getWindowHandles();
+		        for (String window : allWindows) {
+		            if (!window.equals(parentWindow)) {
+		                driver.switchTo().window(window);
+		                break;
+		            }
+		        }
 	    	
 	    	 System.out.println("************************************Series set validation done********************************");
 	
