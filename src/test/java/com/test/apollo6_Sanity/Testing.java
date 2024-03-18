0320ef5716e53e68889525dd0ab175cf547a40ff
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-03-18 09:23:13 +0530

    Testing.java

--- src/test/java/com/test/apollo6_Sanity/Testing.java
+++ src/test/java/com/test/apollo6_Sanity/Testing.java
@@ -34,7 +34,7 @@ public class Testing {
 		{
 			
 			  DesiredCapabilities dc = DesiredCapabilities.chrome();
-		        URL url = new URL("http://172.20.23.7:5555/wd/hub");
+		        URL url = new URL("http://172.12.20.99:4444/wd/hub");
 		        driver = new RemoteWebDriver(url, dc);
 		
 		       
